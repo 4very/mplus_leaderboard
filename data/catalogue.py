6efb137e94129aa84c140e7d067b9a3b2ef1d895
4very <asommer999@gmail.com>
2021-09-12 18:25:01 -0400

    added graphs!

---
+++ data/catalogue.py
@@ -0,0 +1,36 @@
+from WOW import WOW_GetCharData
+from os.path import join, realpath, dirname
+from os import getcwd
+from json import load, dump
+from RIO import RIO_GetCharRankings
+from updateMeta import NumberToClassName, getColorForRunScore, getColorForScore, NumberToClassColor
+
+
+
+
+__location__ = realpath(join(getcwd(), dirname(__file__)))
+
+
+tournFolder = join(__location__, 'pages', 't', 'tournament-2')
+
+
+with open(join(tournFolder, 'teams.json'), 'r') as f:
+  teams = load(f)
+
+with open(join(tournFolder, 'historical.json'), 'r') as f:
+  hist = load(f)
+
+cday = hist['cday']
+ilvlobj = {"day":cday}
+scoreobj = {"day":cday}
+
+for key,item in teams.items():
+  ilvlobj[key] = item["avgilvl"]
+  scoreobj[key] = item["score"]
+  
+hist['ilvl'].append(ilvlobj)
+hist['tscore'].append(scoreobj)
+hist['cday'] += 1
+
+with open(join(tournFolder, 'historical.json'), 'w') as f:
+  dump(hist, f, indent=2)
---
+++ src/components/t/graph.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import { Palette } from '@devexpress/dx-react-chart';
+import {
+  ValueAxis,
+  Chart,
+  LineSeries,
+  Tooltip,
+  ArgumentAxis,
+  Legend,
+} from '@devexpress/dx-react-chart-material-ui';
+import { Typography } from '@material-ui/core';
+
+import Indent from '../misc/indent';
+
+const renderGreyText = (data: any) => {
+  return (
+    <text
+      x={data.x}
+      y={data.y}
+      dy={data.dy}
+      textAnchor={data.textAnchor}
+      className="font-serif text-fontGrey text-lg"
+      style={{
+        fill: '#fffff8',
+      }}
+    >
+      {data.text}
+    </text>
+  );
+};
+
+export default function TournGraph({
+  graphData,
+  header,
+}: {
+  graphData: any;
+  header: String;
+}) {
+  return (
+    <>
+      <Typography variant="h4" className="mt-8 font-serif">
+        {header}
+      </Typography>
+      <Indent className="max-w-7xl mt-4">
+        <Chart data={graphData}>
+          <Palette scheme={['#3B82F6', '#34D399', '#F87171', '#FCD34D']} />
+
+          <ArgumentAxis
+            labelComponent={renderGreyText}
+            // rootComponent={renderTickElement}
+          />
+          {/* <ArgumentAxis /> */}
+          <ValueAxis labelComponent={renderGreyText} showLine />
+
+          <LineSeries name="TTC" valueField="TTC" argumentField="day" />
+          <LineSeries name="ASC" valueField="ASC" argumentField="day" />
+          <LineSeries name="meat" valueField="meat" argumentField="day" />
+          <LineSeries name="WSRF2T" valueField="WSRF2T" argumentField="day" />
+
+          <Legend />
+          <Tooltip />
+        </Chart>
+      </Indent>
+    </>
+  );
+}
