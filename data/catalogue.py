875fb8360cec13b94ea78c0943bfbc74f2c01828
4very <asommer999@gmail.com>
2021-09-14 20:37:41 -0400

    changed to 2x day

--- data/catalogue.py
+++ data/catalogue.py
@@ -30,7 +30,7 @@ for key,item in teams.items():
   
 hist['ilvl'].append(ilvlobj)
 hist['tscore'].append(scoreobj)
-hist['cday'] += 1
+hist['cday'] += 0.5
 
 with open(join(tournFolder, 'historical.json'), 'w') as f:
   dump(hist, f, indent=2)
