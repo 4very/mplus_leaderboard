24d1be1f92104a16f4d0527a3e87ebb96628dbb1
4very <asommer999@gmail.com>
2021-09-20 15:23:06 -0400

    changed dungeon timeers

---
+++ src/pages/update.tsx
@@ -0,0 +1,20 @@
+export default function UpdatePage() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center align-center">
+      <iframe
+        width="1280px"
+        height="720px"
+        className="block"
+        style={{
+          width: '100vw',
+          height: '56.25vw',
+        }}
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ?&autoplay=1"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      />
+    </div>
+  );
+}
