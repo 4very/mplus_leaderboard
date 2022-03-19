f3e4c594f4c53bf4bb95ff34282a0c5ae0c5a0fa
avery <asommer999@gmail.com>
2022-03-19 14:02:30 -0400

    fixed smooth.py

--- data/smooth.py
+++ data/smooth.py
@@ -9,6 +9,7 @@ data = getTournHist('tournament-4')
 for id,e in data.items():
   for i in range(1,len(e)):
     for key, val in e[i].items():
+      if key not in e[i-1]: continue
       if val < e[i-1][key]:
         data[id][i][key] = e[i-1][key]
   
